dd1547e3e12b6ddf7befd953635bf425ac98d12c
LeidyMonge <leidy.mongen@gmail.com>
2022-06-01 13:54:20 -0600

    Unnecessary spaces deleted.

--- Program.cs
+++ Program.cs
@@ -12,11 +12,8 @@ namespace PracticaPOO
             ProdVegetal vegetal = new ProdVegetal("Café", 500, 4);
             Console.WriteLine(vegetal.Nombre);
            
-
             ProdAnimal animal = new ProdAnimal("leche", 100, 1);
             Console.WriteLine(animal.Precio);
-
-           
 
             /*var granja = new Granja("Lecheria Familia Duran", TipoGranja.Lechería, TipoProducto.Leche, 3,3);
             Console.WriteLine(granja.NombreGranja);
